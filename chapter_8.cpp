013b729bf7102af31bca77df0818d5a8cb2f5d68
HeShuHeng <1072436890@qq.com>
2019-09-29 11:38:02 +0800

    int & rats=rat

--- chapter_8.cpp
+++ chapter_8.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+/*
 inline double cube(double);
 
 using namespace std;
@@ -15,4 +15,26 @@ int main()
 inline double cube(double x)
 {
         return x*x*x;
+}
+*/
+
+using namespace std;
+
+void swap(int & a,int &b);
+
+int main()
+{
+        int a=3,b=4;
+        cout<<"a="<<a<<'\t'<<"b="<<b<<endl;
+        swap(a,b);
+        cout<<"a="<<a<<'\t'<<"b="<<b<<endl;
+        return  0;
+}
+
+void swap(int & a,int & b)
+{
+        int c;
+        c=a;
+        a=b;
+        b=c;
 }
